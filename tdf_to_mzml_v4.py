3f0bd6d4e446fb5d3081dc5f761031561cace57f
unknown <gtluu912@gmail.com>
2021-08-10 16:53:38 -0700

    fix test filepath typo

--- tdf_to_mzml_v4.py
+++ tdf_to_mzml_v4.py
@@ -276,9 +276,9 @@ def write_mzml(raw_data, input_filename, output_filename):
 
 if __name__ == "__main__":
     # Read in example .d file and convert to dataframe.
-    pen12_file = 'F:\\code\\alphatims_test\\pen12_ms2_1_36_1_400.d'
+    pen12_file = 'F:\\code\\alphatims_test_data\\pen12_ms2_1_36_1_400.d'
     pen12_df = bruker_to_df(pen12_file)
     write_mzml(pen12_df, pen12_file, 'F:\\code\\alphatims_test_data\\pen12_ms2_1_36_1_400.mzML')
-    bhi_file = 'F:\\code\\alphatims_test\\bhi_ms2_1_32_1_396.d'
+    bhi_file = 'F:\\code\\alphatims_test_data\\bhi_ms2_1_32_1_396.d'
     bhi_df = bruker_to_df(bhi_file)
     write_mzml(bhi_df, bhi_file, 'F:\\code\\alphatims_test_data\\bhi_ms2_1_32_1_396.mzML')

138504f5fedb5fc6be5f67659eb69824935807f4
gtluu <46392631+gtluu@users.noreply.github.com>
2021-11-16 10:35:20 -0800

    Delete prototype directory

--- prototype/run.bat
+++
@@ -1,7 +0,0 @@
-F:\alphatims_test\venv\Scripts\python.exe F:\code\timsconvert\bin\run.py --input F:\code\data\brevi-brachy.d --outdir F:\code\data\test1 --verbose
-F:\alphatims_test\venv\Scripts\python.exe F:\code\timsconvert\bin\run.py --input F:\code\data\meoh_dda_1_1_1_1099.d --outdir F:\code\data\test1 --ms1_groupby scan --verbose
-F:\alphatims_test\venv\Scripts\python.exe F:\code\timsconvert\bin\run.py --input F:\code\data\meoh_dda_1_1_1_1099.d --outdir F:\code\data\test1 --ms1_groupby frame --verbose
-F:\alphatims_test\venv\Scripts\python.exe F:\code\timsconvert\bin\run.py --input F:\code\data\PenMB-ZoneofInhibition-568-TIMS-MSMS.d --outdir F:\code\data\test1 --verbose
-F:\alphatims_test\venv\Scripts\python.exe F:\code\timsconvert\bin\run.py --input F:\code\data\20210920_vc_rugose_1mMTCA_gordon.d --outdir F:\code\data\test1 --verbose
-F:\alphatims_test\venv\Scripts\python.exe F:\code\timsconvert\bin\run.py --input F:\code\data\20210921_vc_rugose_tims_gordon.d --outdir F:\code\data\test1 --verbose
-PAUSE

21d068cb32e3045bb09e85cd2906bf8a722f1b3d
unknown <gtluu912@gmail.com>
2021-11-17 08:44:35 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='timsconvert',
+    version='1.0.0',
+    packages=['timsconvert'],
+    url='https://github.com/gtluu/timsconvert',
+    license='Apache License',
+    author='Gordon T. Luu',
+    author_email='goluu@ucsc.edu',
+    description='TIMSCONVERT: A simple workflow for conversion of trapped ion mobility data to open-source formats'
+)

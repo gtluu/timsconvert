af2cd2a2f167e478ca64f46391ee184e03a48372
unknown <gtluu912@gmail.com>
2022-02-21 17:15:11 -0800

    update validation script

--- bin/run_validation.sh
+++ bin/run_validation.sh
@@ -8,37 +8,37 @@ cd "$parent_path"
 #fi
 
 # test lcms data
-python bin/run.py --input data --experiment lc-tims-ms --outdir data/output --mode raw --verbose
-python bin/run.py --input data --experiment lc-tims-ms --outdir data/output --mode centroid --verbose
-python bin/run.py --input data --experiment lc-tims-ms --outdir data/output --mode profile --verbose
-python bin/run.py --input data --experiment lc-tims-ms --outdir data/output --mode raw --ms2_only --verbose
-python bin/run.py --input data --experiment lc-tims-ms --outdir data/output --mode centroid --ms2_only --verbose
-python bin/run.py --input data --experiment lc-tims-ms --outdir data/output --mode profile --ms2_only --verbose
+python bin/run.py --input data/lcms --outdir data/output/lcms1 --mode raw --verbose
+python bin/run.py --input data/lcms --outdir data/output/lcms2 --mode centroid --verbose
+python bin/run.py --input data/lcms --outdir data/output/lcms3 --mode profile --verbose
+python bin/run.py --input data/lcms --outdir data/output/lcms4 --mode raw --ms2_only --verbose
+python bin/run.py --input data/lcms --outdir data/output/lcms5 --mode centroid --ms2_only --verbose
+python bin/run.py --input data/lcms --outdir data/output/lcms6 --mode profile --ms2_only --verbose
 
 # test maldi dd data
-python bin/run.py --input data --experiment maldi-dd --outdir data/output --mode raw --maldi_output_file combined --verbose
-python bin/run.py --input data --experiment maldi-dd --outdir data/output --mode centroid --maldi_output_file combined --verbose
-python bin/run.py --input data --experiment maldi-dd --outdir data/output --mode profile --maldi_output_file combined --verbose
-python bin/run.py --input data --experiment maldi-dd --outdir data/output --mode raw --maldi_output_file individual --maldi_plate_map --verbose
-python bin/run.py --input data --experiment maldi-dd --outdir data/output --mode centroid --maldi_output_file individual --maldi_plate_map --verbose
-python bin/run.py --input data --experiment maldi-dd --outdir data/output --mode profile --maldi_output_file individual --maldi_plate_map --verbose
-python bin/run.py --input data --experiment maldi-tims-dd --outdir data/output --mode raw --maldi_output_file combined --verbose
-python bin/run.py --input data --experiment maldi-tims-dd --outdir data/output --mode centroid --maldi_output_file combined --verbose
-python bin/run.py --input data --experiment maldi-tims-dd --outdir data/output --mode profile --maldi_output_file combined --verbose
-python bin/run.py --input data --experiment maldi-tims-dd --outdir data/output --mode raw --maldi_output_file individual --maldi_plate_map --verbose
-python bin/run.py --input data --experiment maldi-tims-dd --outdir data/output --mode centroid --maldi_output_file individual --maldi_plate_map --verbose
-python bin/run.py --input data --experiment maldi-tims-dd --outdir data/output --mode profile --maldi_output_file individual --maldi_plate_map --verbose
+python bin/run.py --input data/dd1 --outdir data/output/dd11 --mode raw --maldi_output_file combined --verbose
+python bin/run.py --input data/dd1 --outdir data/output/dd12 --mode centroid --maldi_output_file combined --verbose
+python bin/run.py --input data/dd1 --outdir data/output/dd13 --mode profile --maldi_output_file combined --verbose
+python bin/run.py --input data/dd1 --outdir data/output/dd14 --mode raw --ms2_only --maldi_output_file combined --verbose
+python bin/run.py --input data/dd1 --outdir data/output/dd15 --mode centroid --ms2_only --maldi_output_file combined --verbose
+python bin/run.py --input data/dd1 --outdir data/output/dd16 --mode profile --ms2_only --maldi_output_file combined --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd21 --mode raw --maldi_output_file combined --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd22 --mode centroid --maldi_output_file combined --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd23 --mode profile --maldi_output_file combined --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd24 --mode raw --ms2_only --maldi_output_file combined --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd25 --mode centroid --ms2_only --maldi_output_file combined --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd26 --mode profile --ms2_only --maldi_output_file combined --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd27 --mode raw --maldi_output_file individual --maldi_plate_map plate_map_1.csv --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd28 --mode centroid --maldi_output_file individual --maldi_plate_map plate_map_1.csv --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd29 --mode profile --maldi_output_file individual --maldi_plate_map plate_map_1.csv --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd210 --mode raw --ms2_only --maldi_output_file individual --maldi_plate_map plate_map_1.csv --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd211 --mode centroid --ms2_only --maldi_output_file individual --maldi_plate_map plate_map_1.csv --verbose
+python bin/run.py --input data/dd2 --outdir data/output/dd212 --mode profile --ms2_only --maldi_output_file individual --maldi_plate_map plate_map_1.csv --verbose
 
 # test maldi ims data
-python bin/run.py --input data --experiment maldi-ims --outdir data/output --mode raw --imzml_mode processed --verbose
-python bin/run.py --input data --experiment maldi-ims --outdir data/output --mode centroid --imzml_mode processed --verbose
-python bin/run.py --input data --experiment maldi-ims --outdir data/output --mode profile --imzml_mode processed --verbose
-python bin/run.py --input data --experiment maldi-ims --outdir data/output --mode raw --imzml_mode continuous --verbose
-python bin/run.py --input data --experiment maldi-ims --outdir data/output --mode centroid --imzml_mode continuous --verbose
-python bin/run.py --input data --experiment maldi-ims --outdir data/output --mode profile --imzml_mode continuous --verbose
-python bin/run.py --input data --experiment maldi-tims-ims --outdir data/output --mode raw --imzml_mode processed --verbose
-python bin/run.py --input data --experiment maldi-tims-ims --outdir data/output --mode centroid --imzml_mode processed --verbose
-python bin/run.py --input data --experiment maldi-tims-ims --outdir data/output --mode profile --imzml_mode processed --verbose
-python bin/run.py --input data --experiment maldi-tims-ims --outdir data/output --mode raw --imzml_mode continuous --verbose
-python bin/run.py --input data --experiment maldi-tims-ims --outdir data/output --mode centroid --imzml_mode continuous --verbose
-python bin/run.py --input data --experiment maldi-tims-ims --outdir data/output --mode profile --imzml_mode continuous --verbose
+python bin/run.py --input data/ims --outdir data/output/ims1 --mode raw --imzml_mode processed --verbose
+python bin/run.py --input data/ims --outdir data/output/ims2 --mode centroid --imzml_mode processed --verbose
+python bin/run.py --input data/ims --outdir data/output/ims3 --mode profile --imzml_mode processed --verbose
+python bin/run.py --input data/ims --outdir data/output/ims4 --mode raw --imzml_mode continuous --verbose
+python bin/run.py --input data/ims --outdir data/output/ims5 --mode centroid --imzml_mode continuous --verbose
+python bin/run.py --input data/ims --outdir data/output/ims6 --mode profile --imzml_mode continuous --verbose

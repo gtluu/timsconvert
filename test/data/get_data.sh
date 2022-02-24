0d0df5639b9eb02494242709f115069d82bdc459
unknown <gtluu912@gmail.com>
2022-02-24 13:11:53 -0800

    update validation and data download scripts

--- test/data/get_data.sh
+++ test/data/get_data.sh
@@ -3,5 +3,5 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-wget -r -P "$parent_path" --ftp-user=MSV000088438 --ftp-password=a ftp://massive.ucsd.edu
-wget -r -P "$parent_path" --ftp-user=MSV000084402 --ftp-password=a ftp://massive.ucsd.edu
+wget -r -l=0 -P "$parent_path" --ftp-user=MSV000088438 --ftp-password=a ftp://massive.ucsd.edu
+wget -r -l=0 -P "$parent_path" --ftp-user=MSV000084402 --ftp-password=a ftp://massive.ucsd.edu

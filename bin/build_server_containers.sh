8ba7e3bd6ff1d14da774cd530bf20d182ed9047d
unknown <gtluu912@gmail.com>
2022-04-28 18:29:44 -0700

    fix build container script

--- bin/build_server_containers.sh
+++ bin/build_server_containers.sh
@@ -4,5 +4,5 @@ docker build --tag timsconvert_server -f Dockerfile.server .
 docker build --tag timsconvert_worker -f Dockerfile.worker .
 docker pull redis
 wget -O Dockerfile.rqdashboard https://raw.githubusercontent.com/Parallels/rq-dashboard/master/Dockerfile
-docker build --tag rq_dashboard -f Dockerfile.rqdashboard
+docker build --tag rq_dashboard -f Dockerfile.rqdashboard .
 rm Dockerfile.rqdashboard

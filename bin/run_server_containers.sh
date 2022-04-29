25eaeda22e9977e70020eda44aa476b4e1e2c9c8
unknown <gtluu912@gmail.com>
2022-04-28 18:45:32 -0700

    run redis before worker

--- bin/run_server_containers.sh
+++ bin/run_server_containers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run --rm -it --detach timsconvert_server
+docker run --rm -it --name timsconvert_redis --detach redis
 docker run --rm -it --detach timsconvert_worker
-docker run --rm -it --detach redis
 docker run --rm -it --detach rq_dashboard

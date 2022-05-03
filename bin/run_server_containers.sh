88bb96790fc32e65ec7812bcef2700b57a383553
Mingxun Wang <mwang87@gmail.com>
2022-05-03 16:14:06 -0700

    reorg

--- bin/run_server_containers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it --detach timsconvert_server
-docker run --rm -it --name timsconvert_redis --detach redis
-docker run --rm -it --detach timsconvert_worker
-docker run --rm -it --detach rq_dashboard
--- bin/run_server_worker.py
+++
@@ -1,8 +0,0 @@
-from redis import Redis
-from rq import Queue, Worker, Connection
-
-
-if __name__ == '__main__':
-    with Connection(Redis()):
-        worker = Worker(list(map(Queue, ['default'])))
-        worker.work()

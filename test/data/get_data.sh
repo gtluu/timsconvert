e67cd5319d1cf2a77e52f6a1aa349061d671af3f
unknown <gtluu912@gmail.com>
2022-05-06 14:43:06 -0700

    update local test

--- test/data/get_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-
-wget -r -l=0 -P "$parent_path" --ftp-user=MSV000088438 --ftp-password=a ftp://massive.ucsd.edu
-wget -r -l=0 -P "$parent_path" --ftp-user=MSV000084402 --ftp-password=a ftp://massive.ucsd.edu

72ab1ee83415dd73feec5af734db1d42ba383a35
Mingxun Wang <mwang87@gmail.com>
2022-05-09 12:08:56 -0700

    increasing timeout

--- server/run_server.sh
+++ server/run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python ./main.py
-#gunicorn -w 6 --threads=12 --worker-class=gthread -b 0.0.0.0:5000 --timeout 120 --max-requests 500 --max-requests-jitter 100 --graceful-timeout 120 app:server --access-logfile /app/logs/access.log
+#gunicorn -w 2 --threads=4 --worker-class=gthread -b 0.0.0.0:5000 --timeout 3600 --max-requests 500 --max-requests-jitter 100 --graceful-timeout 3600 app:server --access-logfile /app/logs/access.log

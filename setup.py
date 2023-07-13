1f0b30e601ef304f0e7e83c51b2d43ec5e3898ca
gtluu <46392631+gtluu@users.noreply.github.com>
2023-07-13 13:35:53 -0700

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='timsconvert',
-    version='1.0.1',
+    version='1.0.2',
     packages=['timsconvert'],
     url='https://github.com/gtluu/timsconvert',
     license='Apache License',

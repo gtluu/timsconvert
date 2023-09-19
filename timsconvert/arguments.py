78559dac1528d3c62d2e0a4991c8dfe3ff263e67
bass <gtluu912@gmail.com>
2023-09-19 10:53:14 -0700

    move arg descriptions to json file

--- timsconvert/arguments.py
+++ timsconvert/arguments.py
@@ -1,50 +1,10 @@
 import argparse
+import json
 import os
 import sys
 import logging
 import requests
 from timsconvert.timestamp import *
-
-
-def arg_descriptions():
-    """
-    Return a dict of command line parameters and their descriptions where the keys are arguments and values are
-    parameter descriptions.
-
-    :return: The parameter description dict.
-    :rtype: dict
-    """
-    descriptions = {'input': 'Filepath for Bruker .d file containing TSF or TDF file or directory containing multiple '
-                             'Bruker .d files.',
-                    'outdir': 'Path to folder in which to write output file(s). Default = none',
-                    'outfile': 'User defined filename for output if converting a single file, otherwise files will '
-                               'have same filename and overwrite each other. Default is none.',
-                    'mode': 'Choose whether export to spectra in "raw" or "centroid" formats. Defaults to "centroid".',
-                    'compression': 'Choose between ZLIB compression ("zlib") or no compression ("none"). Defaults to '
-                                   '"zlib".',
-                    'ms2_only': 'Boolean flag that specifies only MS2 spectra should be converted.',
-                    'exclude_mobility': 'Boolean flag used to exclude trapped ion mobility spectrometry data from '
-                                        'exported data. Precursor ion mobility information is still exported.',
-                    'encoding': 'Choose encoding for binary arrays: 32-bit ("32") or 64-bit ("64"). Defaults to '
-                                '64-bit.',
-                    'profile_bins': 'Number of bins used to bin data when converting in profile mode. A value of 0 '
-                                    'indicates no binning is performed. Defaults to 0.',
-                    'barebones_metadata': 'Only use basic mzML metadata. Use if downstream data analysis tools throw '
-                                          'errors with descriptive CV terms.',
-                    'maldi_output_file': 'For MALDI dried droplet data, whether individual scans should be placed in '
-                                         'individual files ("individual"), combined into a single file ("combined"), '
-                                         'or combined by sample label ("sample"). Defaults to "combined".',
-                    'maldi_plate_map': 'Plate map to be used for parsing spots if --maldi_output_file == "individual" '
-                                       'or --maldi_output_file == "sample". Should be a .csv file with no '
-                                       'header/index.',
-                    'imzml_mode': 'Whether .imzML files should be written in "processed" or "continuous" mode. '
-                                  'Defaults to "processed".',
-                    'chunk_size': 'Relative size of chunks of spectral data that are parsed and subsequently written '
-                                  'at once. Increasing parses and write more spectra at once but increases RAM usage. '
-                                  'Default = 10.',
-                    'verbose': 'Boolean flag to determine whether to print logging output.',
-                    'url': 'URL for server to run TIMSCONVERT (if submitting job through API). Default = GNPS server'}
-    return descriptions
 
 
 # Parse arguments for CLI usage
@@ -57,7 +17,8 @@ def get_args(server=False):
     :return: Arguments with default or user specified values.
     :rtype: dict
     """
-    desc = arg_descriptions()
+    with open('timsconvert/parameter_descriptions.json', 'r') as json_file:
+        arg_descriptions = json.loads(json_file.read())
 
     # Initialize parser.
     parser = argparse.ArgumentParser()
@@ -65,59 +26,59 @@ def get_args(server=False):
     # Require Arguments
     required = parser.add_argument_group('Required Parameters')
     required.add_argument('--input',
-                          help=desc['input'],
+                          help=arg_descriptions['input'],
                           required=True,
                           type=str)
 
     # Optional Arguments
     optional = parser.add_argument_group('Optional Parameters')
     optional.add_argument('--outdir',
-                          help=desc['outdir'],
+                          help=arg_descriptions['outdir'],
                           default='',
                           type=str)
     optional.add_argument('--outfile',
-                          help=desc['outfile'],
+                          help=arg_descriptions['outfile'],
                           default='',
                           type=str)
     optional.add_argument('--mode',
-                          help=desc['mode'],
+                          help=arg_descriptions['mode'],
                           default='centroid',
                           type=str,
                           choices=['raw', 'centroid', 'profile'])
     optional.add_argument('--compression',
-                          help=desc['compression'],
+                          help=arg_descriptions['compression'],
                           default='zlib',
                           type=str,
                           choices=['zlib', 'none'])
     optional.add_argument('--ms2_only',
-                          help=desc['ms2_only'],
+                          help=arg_descriptions['ms2_only'],
                           action='store_true')
     optional.add_argument('--exclude_mobility',
-                          help=desc['exclude_mobility'],
+                          help=arg_descriptions['exclude_mobility'],
                           action='store_true')
     optional.add_argument('--encoding',
-                          help=desc['encoding'],
+                          help=arg_descriptions['encoding'],
                           default=64,
                           type=int,
                           choices=[32, 64])
     optional.add_argument('--barebones_metadata',
-                          help=desc['barebones_metadata'],
+                          help=arg_descriptions['barebones_metadata'],
                           action='store_true')
     optional.add_argument('--profile_bins',
-                          help=desc['profile_bins'],
+                          help=arg_descriptions['profile_bins'],
                           default=0,
                           type=int)
     optional.add_argument('--maldi_output_file',
-                          help=desc['maldi_output_file'],
+                          help=arg_descriptions['maldi_output_file'],
                           default='combined',
                           type=str,
                           choices=['combined', 'individual', 'sample'])
     optional.add_argument('--maldi_plate_map',
-                          help=desc['maldi_plate_map'],
+                          help=arg_descriptions['maldi_plate_map'],
                           default='',
                           type=str)
     optional.add_argument('--imzml_mode',
-                          help=desc['imzml_mode'],
+                          help=arg_descriptions['imzml_mode'],
                           default='processed',
                           type=str,
                           choices=['processed', 'continuous'])
@@ -125,16 +86,16 @@ def get_args(server=False):
     # System Arguments
     system = parser.add_argument_group('System Parameters')
     system.add_argument('--chunk_size',
-                        help=desc['chunk_size'],
+                        help=arg_descriptions['chunk_size'],
                         default=10,
                         type=int)
     system.add_argument('--verbose',
-                        help=desc['verbose'],
+                        help=arg_descriptions['verbose'],
                         action='store_true')
     if server:
         # change to GNPS URL later
         system.add_argument('--url',
-                            help=desc['url'],
+                            help=arg_descriptions['url'],
                             default='http://localhost:5000',
                             type=str)
 

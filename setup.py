0b0eca6216f43c930a4c1f2614fdd20739f60eaa
bass <gtluu912@gmail.com>
2023-09-20 13:46:20 -0700

    add code for entrypoint, update api paths, include dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup
+import os
+
+
+if os.path.isfile('requirements.txt'):
+    with open('requirements.txt', 'r') as requirements_file:
+        install_requires = requirements_file.read().splitlines()
+for package in install_requires:
+    if package.startswith('git'):
+        install_requires[install_requires.index(package)] = 'pyimzML @ ' + package
 
 setup(
     name='timsconvert',
     version='1.4.0',
-    packages=['timsconvert'],
     url='https://github.com/gtluu/timsconvert',
     license='Apache License',
     author='Gordon T. Luu',
     author_email='gtluu912@gmail.com',
-    description='TIMSCONVERT: A workflow to convert trapped ion mobility data to open formats'
+    packages=['timsconvert', 'bin', 'lib', 'client', 'docs', 'docsrc', 'server', 'test'],
+    include_package_data=True,
+    package_data={'': ['*.dll', '*.so']},
+    description='TIMSCONVERT: A simple workflow for conversion of trapped ion mobility data to open-source formats',
+    entry_points={'console_scripts': ['timsconvert=bin.run:main']},
+    install_requires=install_requires
 )

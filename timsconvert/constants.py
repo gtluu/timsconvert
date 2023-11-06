f9ec89af5a941558081e835f78b07d316aae9bbb
unknown <gtluu912@gmail.com>
2023-11-06 10:07:55 -0800

    remove unused constants

--- timsconvert/constants.py
+++ timsconvert/constants.py
@@ -6,15 +6,6 @@ logging.info(get_iso8601_timestamp() + ':' + 'Initialize constants...')
 
 VERSION = '1.5.2'
 
-INSTRUMENT_FAMILY = {'1': 'otof',
-                     '2': 'otofq',
-                     '6': 'maxis',
-                     '7': 'impact',
-                     '8': 'compact',
-                     '9': 'timstof',
-                     '512': 'ftms',
-                     '513': 'solarix'}
-
 # Only type "1" has been tested with the current test dataset.
 INSTRUMENT_SOURCE_TYPE = {'0': 'unspecified',
                           '1': 'electrospray ionization',
@@ -28,20 +19,6 @@ INSTRUMENT_SOURCE_TYPE = {'0': 'unspecified',
                           '11': 'CaptiveSpray',
                           '12': 'GC-APCI',
                           '13': 'VIP-HESI-APCI'}
-
-BAF_SCAN_MODE = {'0': 'MS',
-                 '2': 'MS/MS (Auto) | MS/MS (MRM)',
-                 '4': 'MS/MS (isCID)',
-                 '5': 'MS/MS (bbCID)'}
-
-TSF_TDF_SCAN_MODE = {'0': 'MS',
-                     '1': 'MS/MS (Auto)',
-                     '2': 'MS/MS (MRM)',
-                     '4': 'MS/MS (bbCID)',
-                     '8': 'dda-PASEF',
-                     '9': 'dia-PASEF',
-                     '10': 'prm-PASEF',
-                     '20': 'MS/MS (MALDI)'}
 
 MSMS_TYPE = {'0': 'MS',
              '2': 'MS/MS',

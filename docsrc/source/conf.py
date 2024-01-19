57185ba02ab9165436131313188e791a103c89c4
bass <gtluu912@gmail.com>
2024-01-19 08:23:58 -0800

    update documentation

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'TIMSCONVERT'
-copyright = '2023, Gordon T. Luu'
+copyright = '2024, Gordon T. Luu'
 author = 'Gordon T. Luu'
 release = '1.6.3'
 

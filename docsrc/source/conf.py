ed7d80a18709e92e72fbcfde12b01f482bac86f7
gtluu <gtluu912@gmail.com>
2024-03-12 17:48:44 -0500

    fix conflicts

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -13,11 +13,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 project = 'TIMSCONVERT'
 copyright = '2024, Gordon T. Luu'
 author = 'Gordon T. Luu'
-<<<<<<< HEAD
 release = '2.0.0a3'
-=======
-release = '1.6.5'
->>>>>>> 3d6859f11b5475b8412da8f077fe0256132042f4
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
